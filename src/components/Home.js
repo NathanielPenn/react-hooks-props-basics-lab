7fbbaa4959efd3ce3b008abb060bf13bb4a8fe73
NatePennebaker <npenne1@aol.com>
2023-02-02 14:40:08 -0500

    completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function Home(props) {
+  console.log(props);
   return (
     <div id="home">
       <h1 style={{ color: props.color }}>
         {props.name} is a Web Developer from {props.city}
-      </h1>
+      </h1> 
     </div>
   );
 }
